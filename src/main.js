fff45e38792bdc74fe384e36e64a638674716010
Jonas Perusquia Morales <jonaspm_99@outlook.com>
2022-08-31 21:31:16 -0600

    [Changes] - Removed jQuery dependency - Added demo - Support add onPrev, onNext & onImageClick function handlers [Breaking change] - SimpleCarousel constructor now receives a single parameter object

--- src/main.js
+++ src/main.js
@@ -1,102 +1,119 @@
-function SimpleCarousel(id, images, props) {
+class SimpleCarousel {
 
-    /* START FUNCTIONS */
-
-    this.initSetup = function () {
-        this.selectedIndex = 0;
-        this.carousel = $('#' + this.id);
-        
-        this.setupStyles();
-        this.generateCarousel(this.carousel, props);
-        this.fillCarousel(this.content, this.images);
-
-        this.counter.find('.carousel-total').text('' + this.items.length);
-        this.changeCounter(this.counter, this.selectedIndex);
-
-        $('.arrow-next').on('click', function (evt) {
-            self.selectedIndex++;
-            if (self.selectedIndex >= self.items.length) self.selectedIndex = 0;
-            self.items.forEach(function (item, i) {
-                if (i === self.selectedIndex)
-                    item.removeClass('hidden-item');
-                else
-                    item.addClass('hidden-item');
-            });
-            self.changeCounter(self.counter, self.selectedIndex);
-        });
-
-        $('.arrow-prev').on('click', function (evt) {
-            self.selectedIndex--;
-            if (self.selectedIndex < 0) self.selectedIndex = self.items.length - 1;
-            self.items.forEach(function (item, i) {
-                if (i === self.selectedIndex)
-                    item.removeClass('hidden-item');
-                else
-                    item.addClass('hidden-item');
-            });
-            self.changeCounter(self.counter, self.selectedIndex);
-        });
+    constructor(props) {
+        props = props || {}
+        this.id = props.id
+        this.items = []
+        this.height = props.height
+        this.width = props.width
+        this.images = props.images
+        this.onPrev = props.onPrev
+        this.onNext = props.onNext
+        this.onImageClick = props.onImageClick
+        this.initSetup()
     }
 
-    this.fillCarousel = function (content, images) {
-        this.items.length = 0;
-        var itemTemplate = '<div id="$ID" class="carousel-item hidden-item"></div>';
-        for (let i = 0; i < images.length; i++) {
-            self.items.push($(itemTemplate.replace('$ID', 'item-' + i)));
-            if (i == self.selectedIndex)
-                self.items[i].removeClass('hidden-item');
-            self.items[i].css('background-image', "url('" + images[i] + "')");
-            this.setupItemClick(self.items[i], images[i]);
-            content.append(self.items[i]);
+    initSetup() {
+        this.selectedIndex = 0
+        this.carousel = document.querySelector('#' + this.id)
+
+        this.setupStyles();
+        this.generateCarousel(this.carousel)
+        this.fillCarousel(this.content, this.images)
+
+        this.counter.querySelector('.carousel-total').innerText = this.items.length
+        this.changeCounter(this.counter, this.selectedIndex)
+
+        this.carousel.querySelector('.arrow-next').addEventListener('click', evt => {
+            this.selectedIndex++
+            if (this.selectedIndex >= this.items.length)
+                this.selectedIndex = 0
+            this.items.forEach((item, i) => {
+                if (i === this.selectedIndex)
+                    item.classList.remove('hidden-item')
+                else
+                    item.classList.add('hidden-item')
+            });
+            this.changeCounter(this.counter, this.selectedIndex)
+        })
+        this.carousel.querySelector('.arrow-next').addEventListener('click', this.onNext)
+
+        this.carousel.querySelector('.arrow-prev').addEventListener('click', evt => {
+            this.selectedIndex--
+            if (this.selectedIndex < 0)
+                this.selectedIndex = this.items.length - 1
+            this.items.forEach((item, i) => {
+                if (i === this.selectedIndex)
+                    item.classList.remove('hidden-item')
+                else
+                    item.classList.add('hidden-item')
+            })
+            this.changeCounter(this.counter, this.selectedIndex)
+            
+        })
+        this.carousel.querySelector('.arrow-prev').addEventListener('click', this.onPrev)
+    }
+
+    fillCarousel(content, images) {
+        this.items.length = 0
+        var itemTemplate = '<div id="$ID" class="carousel-item hidden-item"></div>'
+        for (const [i,image]  of images.entries()) {
+            this.items.push(
+                this.constructor.createElement(
+                    itemTemplate.replace('$ID', `item-${i}`)
+                )
+            )
+            if (i == this.selectedIndex)
+                this.items[i].classList.remove('hidden-item')
+            this.items[i].style['background-image'] = `url('${image}')`
+            this.setupItemClick(this.items[i], image)
+            content.append(this.items[i])
         }
-    };
+    }
 
-    this.generateCarousel = function (carousel, props) {
-        this.content = $('<div class="carousel-content"></div>');
-        if (props) {
-            this.content.css('height', props.height || '250px');
-            this.content.css('width', props.width || '300px');
-        }
-        this.arrowPrev = $('<a class="arrow arrow-prev"></a>');
-        this.arrowNext = $('<a class="arrow arrow-next"></a>');
-        this.counter = $('<div class="counter"><span class="carousel-index"></span> / <span class="carousel-total"></span></div>');
-        this.content.append(self.arrowPrev);
-        this.content.append(self.arrowNext);
-        this.content.append(self.counter);
-        carousel.append(this.content);
-    };
+    generateCarousel(carousel) {
+        this.content = this.constructor.createElement('<div class="carousel-content"></div>')
+        this.content.style.height = this.height || '250px'
+        this.content.style.width = this.width || '300px'
+        this.arrowPrev = this.constructor.createElement('<a class="arrow arrow-prev"></a>')
+        this.arrowNext = this.constructor.createElement('<a class="arrow arrow-next"></a>')
+        this.counter = this.constructor.createElement('<div class="counter"><span class="carousel-index"></span> / <span class="carousel-total"></span></div>')
+        this.content.appendChild(this.arrowPrev)
+        this.content.appendChild(this.arrowNext)
+        this.content.appendChild(this.counter)
+        carousel.appendChild(this.content)
+    }
 
-    this.changeCounter = function (counter, index) {
-        counter.find('.carousel-index').text(index + 1);
-    };
+    changeCounter(counter, index) {
+        counter.querySelector('.carousel-index').innerText = (index + 1)
+    }
 
-    this.setupItemClick = function (item, imageUrl) {
-        item.on('click', function (evt) {
-            if (self.bigImage)
-                self.removeBigImage();
-            self.bigImage = $('<div class="big-image-bg"><img class="big-image" src="' + imageUrl + '"></div>');
-            self.bigImage.on('click', self.removeBigImage)
-            $('body').append(self.bigImage);
-        });
-    };
+    setupItemClick(item, imageUrl) {
+        item.addEventListener('click', evt => {
+            this.removeBigImage()
+            this.bigImage = this.constructor.createElement('<div class="big-image-bg"><img class="big-image" src="' + imageUrl + '"></div>')
+            document.querySelector('body').appendChild(this.bigImage)
+            this.bigImage.addEventListener('click', () => this.removeBigImage())
+        })
+        item.addEventListener('click', this.onImageClick)
+    }
 
-    this.setupStyles = function() {
+    setupStyles() {
         // TODO
-    };
+    }
 
-    this.removeBigImage = function () {
-        if (self.bigImage) {
-            self.bigImage.remove();
-            delete self.bigImage;
+    removeBigImage() {
+        if (this.bigImage) {
+            this.bigImage.remove()
+            delete this.bigImage
         }
-    };
+    }
 
-    /* END FUNCTIONS */
+    static createElement(strHTML) {
+        let template = document.createElement('template');
+        strHTML = strHTML.trim(); // Never return a text node of whitespace as the result
+        template.innerHTML = strHTML;
+        return template.content.firstChild;
+    }
 
-    var self = this;
-    this.id = id;
-    this.items = [];
-    this.images = images;
-    props = props || {};
-    this.initSetup();
 }
